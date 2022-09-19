d4e71de9d098893646cd1574e9c9e1b07df5733d
Prakash8782 <prakash8782@gmail.com>
2022-09-19 15:43:29 +0530

    push

---
+++ push/main.js
@@ -0,0 +1,7 @@
+let colors=["Red","Blue","Green","Yellow","Pink"];
+console.log("colors : ",colors);
+console.log("length of an array : "+colors.length);
+console.log("method : array.push()");
+colors.push("Black");
+console.log("change in array :",colors);
+console.log("return : length of array after push(); is "+colors.length);
